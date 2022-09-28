7a2a2080a679c664c692f0d61b6004977d7203f3
urita-fuki <nogihina0809@i.softbank.jp>
2022-09-28 11:38:05 +0900

    p209

--- journal/views.py
+++ journal/views.py
@@ -1,6 +1,11 @@
+#import logging
+
 from django.shortcuts import render
 from django.views import generic
+#from django.urls import reverse_lazy
+#from django.contrib import messages
 from .forms import InquiryForm
+
 
 # Create your views here.
 class IndexView(generic.TemplateView):
@@ -9,3 +14,10 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    #success_url = reverse_lazy('dinary:inquiry')
+
+    '''def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)'''
